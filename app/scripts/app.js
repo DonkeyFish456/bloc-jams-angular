0904122700970a1c7866eb481f4e158b58bf33b0
DonkeyFish456 <stancalderon@gmail.com>
2016-09-15 19:57:20 -0700

    add controllers for landing, collection, and album pages. Adjust templates to incorporate contrllers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,14 +8,17 @@ function config($stateProvider, $locationProvider) {
     $stateProvider
         .state('landing', {
             url: '/',
+            controller: 'LandingCtrl as landing',
             templateUrl: '/templates/landing.html'
         })
         .state('album', {
             url: '/album',
+            controller: 'AlbumCtrl as album',
             templateUrl: '/templates/album.html'
         })
         .state('collection',{
             url: '/collection',
+            controller: 'CollectionCtrl as collection',
             templateUrl: '/templates/collection.html'
         });
 
