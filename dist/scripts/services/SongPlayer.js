ee461829b2c6febcd81128ab8106d56ca3e78620
DonkeyFish456 <stancalderon@gmail.com>
2016-09-18 16:13:14 -0700

    add playerbar functionality for play, pause, previous, and next buttons. Song data is now dynamic.

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,7 +1,13 @@
 (function () {
-    function SongPlayer() {
+    function SongPlayer(Fixtures) {
         var SongPlayer = {};
-        var currentSong = null;
+        /**
+         * @desc Sets current album globally
+         * @param {Object}
+         */
+        var currentAlbum = Fixtures.getAlbum()
+
+
         /**
          * @desc Buzz object audio file
          * @type {Object}
@@ -15,14 +21,15 @@
         var setSong = function (song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentBuzzObject.playing = null;
+                SongPlayer.currentSong.playing = null;
+
             }
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
             });
 
-            currentSong = song;
+            SongPlayer.currentSong = song;
         };
         /**
          * @function playSong
@@ -34,16 +41,39 @@
             song.playing = true;
         };
         /**
+         * @function stopSong
+         * @desc stop the currentBuzzObject and set the playing property of song to false
+         * @param {Object} song
+         */
+        var stopSong = function (song) {
+            currentBuzzObject.stop();
+            song.playing = false;
+        };
+        /**
+         * @desc Get index of current song
+         * @type {Object}
+         */
+        var getSongIndex = function (song) {
+            return currentAlbum.songs.indexOf(song)
+        }
+
+        /**
+         * @desc Active song object from list of songs
+         * @type {Object}
+         */
+        SongPlayer.currentSong = null;
+        /**
          * @function SongPlayer.play
-         * @desc public method to test the state of currentSong and play song when called
+         * @desc public method to test the state of currentSong and play song
          * @param {Object} song
          */
 
         SongPlayer.play = function (song) {
-            if (currentSong !== song) {
+            song = song || SongPlayer.currentSong;
+            if (SongPlayer.currentSong !== song) {
                 setSong(song);
                 playSong(song);
-            } else if (currentSong === song){
+            } else if (SongPlayer.currentSong === song){
                 if (currentBuzzObject.isPaused()) {
                     playSong(song);
                 }
@@ -51,13 +81,41 @@
         };
         /**
          * @function SongPlayer.play
-         * @desc public method to test the state of currentSong and pause song when called
+         * @desc public method to test the state of currentSong and pause song
          * @param {Object} song
          */
         SongPlayer.pause = function (song) {
+            song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
             song.playing = false;
         };
+        /**
+         * @function SongPlayer.previous
+         * @desc public method to to switch to previous song
+         * @param {Object} song
+         */
+        SongPlayer.previous = function () {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+
+            currentSongIndex = (currentAlbum.songs.length + currentSongIndex - 1)%currentAlbum.songs.length;
+            var song = currentAlbum.songs[currentSongIndex];
+            setSong(song);
+            playSong(song);
+
+        };
+        /**
+         * @function SongPlayer.next
+         * @desc public method to to switch to the next song
+         * @param {Object} song
+         */
+        SongPlayer.next = function () {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex = (currentAlbum.songs.length + currentSongIndex + 1)%currentAlbum.songs.length;
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song)
+        };
+
 
         return SongPlayer;
     }
